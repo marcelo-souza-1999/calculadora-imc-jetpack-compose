b8ac8e0a4b8ae15f55d7438931c8cb708bae2f2e
marcelo-souza-1999 <developermarcelosouza@gmail.com>
2024-07-13 19:28:50 -0300

    setup: first commit

---
+++ app/src/main/java/com/marcelos/calculadoraimc/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.marcelos.calculadoraimc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import com.marcelos.calculadoraimc.ui.theme.CalculadoraIMCTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CalculadoraIMCTheme {
+                SetupLayout()
+            }
+        }
+    }
+
+    @Composable
+    private fun SetupLayout() {
+
+    }
+
+}
---
+++ app/src/main/java/com/marcelos/calculadoraimc/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.marcelos.calculadoraimc.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val LightBlue = Color(0XFF2196F3)
+val DarkBlue = Color(0XFF11568D)
+val White = Color(0XFFFFFFFF)
